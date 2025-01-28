80013ef90fe29171433662fb0461f213104df50f
Roberto <robertosantiago@med-snap.com>
2025-01-28 12:05:36 +0000

    create and separate css files

--- backend/settings.py
+++ backend/settings.py
@@ -146,6 +146,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # If you have global static files
+]
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
